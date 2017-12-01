b167bd162fde115b5ee89c9ed917a77633b9af53
Jaime Gomez <jgomez@sproutloud.com>
2017-12-01 16:36:00 -0500

    React desde 0 - routes dom - Setting up the routes

---
+++ src/views/ContactUs.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ConctactUs extends Component {
+    render() {
+        return (
+            <div className="ConctactUs">
+                <h1>ConctactUs</h1>
+            </div>
+        );
+    }
+}
+
+export default ConctactUs;
